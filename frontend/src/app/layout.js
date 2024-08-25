313959cdfba3717e1a224a1046dd2806ea64daf2
FreelancerMamun <mamunkhan16345@gmail.com>
2024-08-25 19:30:50 +0600

    add new  page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-
 const inter = Inter({ subsets: ['latin'] });
+
+import Header from '@/sections/header';
+import Footer from '@/sections/Footer';
 
 export const metadata = {
   title: 'Find A New Job',
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header></Header>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   );
 }
---
+++ frontend/src/components/button.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+const Button = () => {
+  return <button className=" text-xl">Button</button>;
+};
+
+export default Button;
