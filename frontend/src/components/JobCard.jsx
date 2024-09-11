61b462e395343c4370c4f6d863307e66776b45b1
FreelancerMamun <mamunkhan16345@gmail.com>
2024-09-11 22:54:34 +0600

    update job card

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const JobCard = () => {
   return (
-    <div className=" bg-[#F2F6FD] border p-4 ">
+    <div className=" bg-[#F2F6FD] border rounded-md hover:shadow-[0_10px_20px_-5px_rgba(10,42,105,.06)] p-4 hover:bg-transparent duration-300 transition  hover:border-[#b4c0e0]">
       <div className="px-2 pt-10 pb-5">
-        <h4 className="font-semibold">UiUX Designer</h4>
+        <h4 className="font-semibold text-xl">UiUX Designer</h4>
         <div className="flex gap-2 mt-1">
           <div className="time">
             <p className="text-[#a0abb8] font-light text-sm">Full time</p>
@@ -15,7 +15,7 @@ const JobCard = () => {
             </p>
           </div>
         </div>
-        <div className="job-discription my-3">
+        <div className="job-discription my-8">
           <p className="text-[15px]  text-[#4f5e64]">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
             architecto eveniet, dolor quo repellendus pariatur
